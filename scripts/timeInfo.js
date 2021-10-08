b60eecdb212e8a9eb2fbb92af044080cba40f418
seo.changmin.dev <seo.changmin.dev@gmail.com>
2021-10-08 22:43:45 +0900

    Add meal provider section in each place for meal

--- scripts/timeInfo.js
+++ scripts/timeInfo.js
@@ -1,6 +1,6 @@
 const month = ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'];
 const day = ['일요일','월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
-const meal = ['<아침>', '<점심>', '<저녁>'];
+const meal = ['[아침]', '[점심]', '[저녁]'];
 
 const today = new Date();
 const bigDate = month[today.getMonth()] + ' ' + today.getDate() + '일' + ' ' + day[today.getDay()] + ' ';
