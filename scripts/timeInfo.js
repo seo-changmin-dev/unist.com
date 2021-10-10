9b0741ea3cc1dd4185215efbbda2021b29899e6c
seo.changmin.dev <seo.changmin.dev@gmail.com>
2021-10-10 21:55:40 +0900

    Implement day and date change when time is after 20:00

--- scripts/timeInfo.js
+++ scripts/timeInfo.js
@@ -2,19 +2,25 @@ const month_arr = ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월
 const day_arr = ['일요일','월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
 const meal_arr = ['[조식]', '[중식]', '[석식]'];
 
-const today = new Date();
+let today = new Date();
 const month = today.getMonth();
-const date = today.getDate();
-const day = today.getDay();
+let date = today.getDate();
+let day = today.getDay();
 const hour = today.getHours();
 const minute = today.getMinutes();
 
 
 const mealTime = document.querySelector('.time_info > .meal')
 // 아침 : 전날 20시~10시 점심 :10~14 저녁: 14:00 ~ 20:00
-if (10 <= hour && hour <= 14) mealTime.innerText = meal_arr[1];
-else if(14 < hour && hour <= 20) mealTime.innerText = meal_arr[2];
+if (10 <= hour && hour < 14) mealTime.innerText = meal_arr[1];
+else if(14 <= hour && hour <= 20) mealTime.innerText = meal_arr[2];
 else mealTime.innerText = meal_arr[0];
+
+if (hour >= 20) {
+    today.setDate(today.getDate() + 1);
+    day = today.getDay();
+    date = today.getDate();
+}
 
 const bigDate = month_arr[month] + ' ' + date + '일' + ' ' + day_arr[day] + ' ';
 
