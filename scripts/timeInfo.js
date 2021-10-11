0ab25d85f07dce99b7f865cc86e4ab32f5d3cf3a
seo.changmin.dev <seo.changmin.dev@gmail.com>
2021-10-11 20:35:10 +0900

    repair date error
    20시 이후에는 다음날 아침을 보여줘야 하는데,  단순히 date + 1 만으로는 안돼서, 일단 미봉책으로 오늘 날짜를 안 보여주게 설정했다.

--- scripts/timeInfo.js
+++ scripts/timeInfo.js
@@ -9,21 +9,57 @@ let day = today.getDay();
 const hour = today.getHours();
 const minute = today.getMinutes();
 
-
 const mealTime = document.querySelector('.time_info > .meal')
+
 // 아침 : 전날 20시~10시 점심 :10~14 저녁: 14:00 ~ 20:00
 if (10 <= hour && hour < 14) mealTime.innerText = meal_arr[1];
-else if(14 <= hour && hour <= 20) mealTime.innerText = meal_arr[2];
+else if(14 <= hour && hour < 20) mealTime.innerText = meal_arr[2];
 else mealTime.innerText = meal_arr[0];
-
-if (hour >= 20) {
-    today.setDate(today.getDate() + 1);
-    day = today.getDay();
-    date = today.getDate();
-}
 
 const bigDate = month_arr[month] + ' ' + date + '일' + ' ' + day_arr[day] + ' ';
 
 const currentTime = document.querySelector('.time_info > .date');
-currentTime.innerText =  bigDate;
+//currentTime.innerText = bigDate;
+currentTime.innerText = '';
 
+// 0 = 조식, 1 = 중식, 2 = 석식
+let eatHour = 0;
+if (10 <= hour && hour < 14) eatHour = 1;
+else if(14 <= hour && hour < 20) eatHour = 2;
+
+if (hour >= 20) {
+  day = (day + 1) % 7;
+}
+
+// 긱식 업데이트
+const giksik = document.querySelector('.giksik').querySelectorAll('.menu');
+for(let place = 0; place < 2; place++) {
+
+  for(let i = 0, menuLen = weekly_giksik[day][eatHour][place].length; i < menuLen; i++) {
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(weekly_giksik[day][eatHour][place][i]));
+    giksik[place].appendChild(li);
+  }
+}
+
+// 학식 업데이트
+const haksik = document.querySelector('.haksik').querySelectorAll('.menu');
+for(let place = 0; place < 2; place++) {
+
+  for(let i = 0, menuLen = weekly_haksik[day][eatHour][place].length; i < menuLen; i++) {
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(weekly_haksik[day][eatHour][place][i]));
+    haksik[place].appendChild(li);
+  }
+}
+
+// 교식 업데이트
+const kyosik = document.querySelector('.kyosik').querySelectorAll('.menu');
+for(let place = 0; place < 1; place++) {
+
+  for(let i = 0, menuLen = weekly_kyosik[day][eatHour][place].length; i < menuLen; i++) {
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(weekly_kyosik[day][eatHour][place][i]));
+    kyosik[place].appendChild(li);
+  }
+}
